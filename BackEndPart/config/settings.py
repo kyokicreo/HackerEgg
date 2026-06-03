3a5b01cf49354bf17b4639ee462cba148bee26af
kyokicreo <kyokicreo@gmail.com>
2026-06-03 16:55:50 +0600

    new

--- BackEndPart/config/settings.py
+++ BackEndPart/config/settings.py
@@ -7,7 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/6.0/topics/settings/
 
 For the full list of settings and their values, see
-https://docs.djangoproject.com/en/6.0/ref/settings/
+hi
 """
 
 from pathlib import Path
